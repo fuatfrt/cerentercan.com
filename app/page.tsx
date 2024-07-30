b6ca01d5cbbd42614bc99cf44ae8e084b8efb0db
fuatfrt <fuatfrt44@gmail.com>
2024-07-30 19:16:59 +0300

    Press.tsx ve SSS.tsx eklendi

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
 import Hero from "@/app/ui/Home/Hero";
-import Content from "@/app/ui/Home/Content";
 import Services from "@/app/ui/Home/Services";
 import Footer from "@/app/ui/Footer";
 import About from "@/app/ui/Home/About";
 import Contact from "@/app/ui/Home/Contact";
+import Sss from "@/app/ui/SSS";
+import Press from "@/app/ui/Press";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-full w-full">
       <Hero />
       <About id="hakkımda" />
-      <Content />
       <Services />
+      <Press id="basin" />
+      <Sss id="sss" />
       <Contact />
       <Footer />
     </div>
