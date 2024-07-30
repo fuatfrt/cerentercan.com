0c47e027f96fe52388c3a8c0424fc1d45640fcd0
fuatfrt <fuatfrt44@gmail.com>
2024-07-30 19:11:29 +0300

    Accordion.tsx component'i için item component'i

---
+++ app/ui/AccordionItem.tsx
@@ -0,0 +1,32 @@
+import {IoIosArrowDropdown} from "react-icons/io";
+import React from "react";
+import AccordionDesc from "@/app/ui/AccordionDesc";
+
+interface AccordionItem {
+  index: number;
+  title: string;
+  open: number | null;
+  activeItem: Function;
+  children: React.ReactNode;
+}
+export default function AccordionItem({index, title, open, activeItem, children}: AccordionItem) {
+  return (
+    <>
+      <button
+        onClick={() => activeItem(index)}
+        className="flex flex-row justify-between items-center w-full border-b border-black">
+        <h2 className="md:text-xl font-bold text-nowrap">{title}</h2>
+        <span className={`${
+          open === index
+            ? "rotate-180"
+            : ""
+        } flex justify-center items-center h-full font-black duration-5000 transform`}>
+          <IoIosArrowDropdown size="25px" className="font-bold"/>
+        </span>
+      </button>
+      <AccordionDesc index={index} open={open}>
+        {children}
+      </AccordionDesc>
+    </>
+  );
+}
