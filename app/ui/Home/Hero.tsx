8912ff24e2f4e88c55324f2d60bdd4d078a227a5
fuatfrt <fuatfrt44@gmail.com>
2024-07-31 11:40:00 +0300

    giriş yazısı düzenlendi.

--- app/ui/Home/Hero.tsx
+++ app/ui/Home/Hero.tsx
@@ -5,12 +5,15 @@ export default function Hero() {
         <div className="relative flex justify-center items-center h-full w-full max-w-4xl text-black">
           <div
             className="absolute flex flex-col w-full bottom-0 left-0 right-0 gap-4 p-4 bg-white opacity-75 rounded-t-lg z-30">
-            <h1 className="text-lg md:text-2xl font-black text-center">❝Kendinizin En İyi Versiyonu Olun❞</h1>
-            <p className="text-l md:text-xl text-justify font-bold">
-              Hoş geldiniz! Burası, gerçek potansiyelinizi keşfetmenin heyecan dolu bir yolculuğu. Mindfulness,
-              psikoloji, nefes, beden ve dans gibi güçlü araçlarla desteklenen bir koçluk süreci sunuyorum. İçsel
-              dengeyi bulma, İlişkileri iyileştirme, tutkularınızı takip etme ve yaşamınızı yeniden yapılandırma yolunda
-              sizi rehberlik etmek için buradayım. Potansiyellerinizi keşfedin, yaşamınızı en yüksek kaliteden yaşayın.
+            <h1 className="md:text-2xl font-black text-center">❝Yeniden Başlasan Kim Olurdun❞</h1>
+            <p className="font-light text-center">
+              Rotanı nereye çevirirsen o tarafa gidersin.Lakin bazen rüzgara göre şekil alman gerekebilir
+            </p>
+            <p className="md:text-xl text-center font-bold">
+              Mindfulness, psikoloji, nefes, beden ve dans gibi güçlü araçlarla desteklenen danışmanlık süreci. İçsel dengeyi sağlama, İlişkileri iyileştirme, sınav dönemi desteği, sağlıklı YKS-LGS tercihleri, tutkularınızı takip etme ve yaşamınızı yeniden yapılandırma yolunda profesyonel rehberlik hizmeti.
+            </p>
+            <p className="md:text-xl font-bold text-center">
+              Potansiyellerini keşfet, rotanı planla, yaşamını yeniden oluştur.
             </p>
           </div>
         </div>
