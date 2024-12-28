e5372956f426e9352b9cf05d9319d72886b3d150
fuatfrt <fuatfrt44@gmail.com>
2024-12-28 19:16:02 +0300

    Yazılar değiştirildi.

--- app/ui/Home/About.tsx
+++ app/ui/Home/About.tsx
@@ -9,24 +9,13 @@ export default function About({id}: { id: string }) {
           <h2 className="font-extralight">HOLİSTİK DANIŞMAN ve TERAPİ UZMANI</h2>
           <div className="flex flex-col max-w-2xl gap-4">
             <p>
-              Ben Ceren TERCAN, 2012 yılından beri Rehber Öğretmen ve Psikolojik Danışman olarak profesyonel danışmanlık
-              hizmeti vermekteyim. Okul çağı ( 3-25 yaş) ve yetişkin gruplarıyla kapsamlı çalışmalar yürütüyorum.
-              Mesleğime farklı bir perspektif sunması amacıyla Travma Terapisti David CORNWELL ve Nörobilim Uzmanı PPC
-              Banu ÇEÇEN’in beraber yürüttüğü “ICF Mindfulness Koçluk Programı” ve Uzm. Klinik Psikolog Gökhan ÇINAR’ın
-              464 saatlik “Aile Danışmanlığı Eğitimi”ni tamamladım. Uzman Sosyolog olarak mesleğimi yürütmenin yanı sıra
-              yetişkin gruplarına Mindfulness Koçluğu ve Aile Danışmanlığı yapmaktayım. İnsan psikolojisi üzerindeki
-              olumlu dönüşümü bilimsel olarak kanıtlanan mindfulness, dans, yoga, nefes ve beden farkındalığı
-              alanlarından beslenerek birleştirdiğim, özgün grup atölyeleri ve terapi seansları vermekteyim.
+              Ben Ceren TERCAN, 2012 yılından beri Rehber Öğretmen ve Psikolojik Danışman olarak profesyonel danışmanlık hizmeti vermekteyim. Okul öncesi dönemden, yetişkinlere kadar tüm yaş gruplarıyla kapsamlı çalışmalar yürütüyorum. Mesleğime farklı bir perspektif sunması amacıyla Travma Terapisti David CORNWELL ve Nörobilim Uzmanı PPC Banu ÇEÇEN’in beraber yürüttüğü “ICF Mindfulness Koçluk Programı” ve Uzm. Klinik Psikolog Gökhan ÇINAR’ın 464 saatlik “Aile Danışmanlığı Eğitimi”ni tamamladım. Uzman Sosyolog olarak mesleğimi yürütmenin yanı sıra yetişkin gruplarına Mindfulness Koçluğu ve Aile Danışmanlığı yapmaktayım. Çocuklardan yetişkinlere profesyonel danışmanlık hizmeti sunarken mindfulness, dans, yoga, nefes ve beden farkındalığı teknikleri ile çalışmalarımı destekliyorum.
             </p>
             <p>
-              Misyonum: Bireylerin zihinsel, duygusal, fiziksel sağlıklarını ve ilişkilerini iyileştirmeleri, sağlıklı
-              karar mekanizmaları oluşturmaları adına; zihin, nefes ve beden farkındalığı üzerine yoğunlaşarak kişilerin
-              öncelikle kendileriyle olan iletişimini güçlendirerek daha kaliteli bir yaşam kurmaları adına
-              danışanlarıma profesyonel destek sağlamak.
+              Misyonum: Psikolojik ekolleri temele alarak; Zihin, nefes ve beden farkındalığı teknikleriyle bireylerin; zihinsel, duygusal, fiziksel sağlıklarını ve ilişkilerini iyileştirmeleri, mutlu bir yaşam kurmaları adına danışanlarıma profesyonel destek sağlamak.
             </p>
             <p>
-              Siz de ilişkilerinizi iyileştirmek, stresinizi yönetmek, hayatınıza yön vermek, zihinsel ve duygusal
-              açıdan dengelenmek için kendinize adım atmak isterseniz benimle iletişime geçebilirsiniz.
+              İlişkilerinizi iyileştirmek, sürekli yaşadığınız döngüleri değiştirmek, hayatınıza yeni bir yön vermek, zihinsel ve duygusal açıdan dengelenmek için adım atmak isterseniz benimle iletişime geçebilirsiniz.
             </p>
           </div>
           <AccordionButton/>
