c66ca223a489929f2b78afacdabd9385dc1d5e36
fuatfrt <fuatfrt44@gmail.com>
2024-12-28 19:51:08 +0300

    Komponentin görünümü düzenlendi

--- app/ui/components/CarouselComponent.tsx
+++ app/ui/components/CarouselComponent.tsx
@@ -9,18 +9,18 @@ import {PressMediaInterface} from "@/lib/datas";
 
 export function CarouselComponent({items}: {items: PressMediaInterface[]}) {
   return (
-    <div className="flex justify-center items-center w-full">
+    <div className="flex justify-center items-center w-full px-14">
       <Carousel
         opts={{
           align: "start",
         }}
-        className="flex justify-center items-center w-full max-w-sm sm:max-w-lg lg:max-w-7xl mx-10"
+        className="flex justify-center items-center w-full max-w-sm sm:max-w-lg lg:max-w-7xl"
       >
         <CarouselContent>
           {items.map((item, index) => (
             <CarouselItem key={index} className="flex w-full lg:basis-1/2 xl:basis-1/3">
-              <div className="">
-                <div className="flex aspect-square items-center justify-center p-6">
+              <div className="flex flex-col w-full">
+                <div className="flex aspect-square items-center justify-center">
                   <img className="w-full" src={item.image.link} alt={item.image.alt}/>
                 </div>
                 <div className="flex flex-col w-full gap-2">
