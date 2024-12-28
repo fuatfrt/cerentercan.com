955a418ab89b6224e887ce1751172a6b53d9f154
fuatfrt <fuatfrt44@gmail.com>
2024-12-28 19:18:20 +0300

    shdcn carousel componenti eklendi

---
+++ app/ui/components/CarouselComponent.tsx
@@ -0,0 +1,39 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import {PressMediaInterface} from "@/lib/datas";
+
+export function CarouselComponent({items}: {items: PressMediaInterface[]}) {
+  return (
+    <div className="flex justify-center items-center w-full">
+      <Carousel
+        opts={{
+          align: "start",
+        }}
+        className="flex justify-center items-center w-full max-w-sm sm:max-w-lg lg:max-w-7xl mx-10"
+      >
+        <CarouselContent>
+          {items.map((item, index) => (
+            <CarouselItem key={index} className="flex w-full lg:basis-1/2 xl:basis-1/3">
+              <div className="">
+                <div className="flex aspect-square items-center justify-center p-6">
+                  <img className="w-full" src={item.image.link} alt={item.image.alt}/>
+                </div>
+                <div className="flex flex-col w-full gap-2">
+                  <h1 className="font-semibold text-center">{item.title}</h1>
+                  <p>{item.text}</p>
+                </div>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious/>
+        <CarouselNext/>
+      </Carousel>
+    </div>
+  )
+}
