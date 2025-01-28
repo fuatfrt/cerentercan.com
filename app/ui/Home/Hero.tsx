4dee711a09e797a137e1476beb3346df1fe37645
fuatfrt <fuatfrt44@gmail.com>
2025-01-28 19:40:41 +0300

    Yazının olduğu alan yeniden düzenlendi

--- app/ui/Home/Hero.tsx
+++ app/ui/Home/Hero.tsx
@@ -3,13 +3,14 @@ export default function Hero() {
     <div className="flex flex-col justify-center items-center h-[84vh] lg:h-[95vh] w-full">
       <div className="relative flex justify-center items-center h-full w-full overflow-hidden">
         <div className="relative flex justify-center items-center h-full w-full max-w-4xl text-black">
-          <div
-            className="absolute flex flex-col h-fit w-full bottom-0 left-0 right-0 gap-4 p-4 bg-white opacity-65 rounded-t-lg z-30">
-            <h1 className="md:text-2xl font-black text-center">❝Yeniden Başlasan Kim Olurdun❞</h1>
-            <p className="font-semibold text-center">
-              Rotanı nereye çevirirsen o tarafa gidersin, lakin bazen rüzgâra göre şekil alman gerekebilir.
-              Karşına çıkan rüzgarın, seni planladığın noktadan daha iyi bir yere götürebileceğini bilseydin, karşına çıkan rüzgara hissin ne olurdu?
-            </p>
+          <div className="absolute flex flex-col h-fit w-full bottom-4 left-0 right-0 gap-4 p-4 rounded-lg z-30">
+            <div className="bg-white bg-opacity-85 p-4 rounded-lg">
+              <h1 className="md:text-2xl font-black text-center">❝Yeniden Başlasan Kim Olurdun❞</h1>
+              <p className="font-semibold text-center">
+                Rotanı nereye çevirirsen o tarafa gidersin, lakin bazen rüzgâra göre şekil alman gerekebilir.
+                Karşına çıkan rüzgarın, seni planladığın noktadan daha iyi bir yere götürebileceğini bilseydin, karşına çıkan rüzgara hissin ne olurdu?
+              </p>
+            </div>
           </div>
         </div>
         <img
